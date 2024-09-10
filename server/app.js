4a97b1350f302d82044e17a3a9653801ad2f3b0e
Abstract Programmer <AbstractProgrammer@example.com>
2024-09-10 18:59:18 +0530

    Initial commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 const classroomRouter = require('./routes/classrooms');
 
 //const url = 'mongodb://127.0.0.1:27017/MGIT';
-const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+//const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+const url='mongodb+srv://varshithanuli22:varshithanuli22@merncrud.qslno.mongodb.net/?retryWrites=true&w=majority&appName=MernCrud';
 const app = express();
 mongoose.connect(url);
 const con = mongoose.connection;
@@ -13,7 +14,7 @@ con.on('open', () => {
     console.log('connected...');
 });
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 
 app.use('/classrooms', classroomRouter);
